bfe258d69b912cf42cc454d2820d0a5b2f2c6bfe
risharay <rayrisha8@gmail.com>
2020-02-26 14:52:41 -0800

    Adding features: user can tap a cell to see more details about a particular movie user can tap a tab bar button to view a grid layout of Movie Posters using a CollectionView

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Risha Ray on 2/26/20.
+//  Copyright © 2020 Risha Ray. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -12,6 +12,7 @@ import AlamofireImage
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     
+    
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,7 +22,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         tableView.dataSource = self
         tableView.delegate = self
-        print("Hello")
+        
 
         // Do any additional setup after loading the view.
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
@@ -38,7 +39,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
             
             self.tableView.reloadData()
             
-            print(dataDictionary)
+            
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
               // TODO: Reload your table view data
@@ -78,14 +79,29 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        
+        print("Loading up the details screen")
+        
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
